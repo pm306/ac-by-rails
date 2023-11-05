1015b83b8efd023f20b7aaed4248c667ea4eaa79
pm <ampm65535@gmail.com>
2023-11-05 19:02:13 +0900

    戻るボタン追加

--- spec/features/page_navigation_spec.rb
+++ spec/features/page_navigation_spec.rb
@@ -7,11 +7,10 @@ RSpec.feature "ページ遷移のテスト", type: :feature do
         expect(current_path).to eq(signup_path)
     end
 
-    #遷移先のトップページが未実装なのでコメントアウト
-    # scenario "ユーザー登録→トップに遷移する" do
-    #     visit login_path
-    #     click_link "戻る"
-    #     expect(current_path).to eq(signup_path)
-    # end    
+    scenario "ユーザー登録→ログインに遷移する" do
+        visit signup_path
+        click_link "戻る"
+        expect(current_path).to eq(login_path)
+    end    
 end
 
