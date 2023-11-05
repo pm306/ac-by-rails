a42276cd9f42854a8240501e17ed6e086c1c961c
pm <ampm65535@gmail.com>
2023-11-05 15:25:19 +0900

    ユーザー登録成功でメッセージを表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "ユーザー登録に成功しました！"
       redirect_to login_path
     else
       render :new
