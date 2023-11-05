1153cb3a9137033f380f2e2b42aafec72e854e90
pm <ampm65535@gmail.com>
2023-11-05 14:23:56 +0900

    ユーザー登録フォームの作成／表示テスト

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new', as: 'signup'
   get '/login', to: 'sessions#new', as: 'login'
 
+  resources :users #usersヘルパーが利用可能になる
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
--- spec/features/page_navigation_spec.rb
+++ spec/features/page_navigation_spec.rb
@@ -6,4 +6,12 @@ RSpec.feature "ページ遷移のテスト", type: :feature do
         click_link "ユーザー登録"
         expect(current_path).to eq(signup_path)
     end
+
+    #遷移先のトップページが未実装なのでコメントアウト
+    # scenario "ユーザー登録→トップに遷移する" do
+    #     visit login_path
+    #     click_link "戻る"
+    #     expect(current_path).to eq(signup_path)
+    # end    
 end
+
