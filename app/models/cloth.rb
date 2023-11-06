6a720cf26ef5209febdddea65a375f951f5059a9
pm <ampm65535@gmail.com>
2023-11-06 23:14:15 +0900

    モデルの設定、バリデーションチェック、キーの関連付け

--- app/models/cloth.rb
+++ app/models/cloth.rb
@@ -1,2 +1,28 @@
 class Cloth < ApplicationRecord
+    belongs_to :user
+    belongs_to :cloth_type, primary_key: :type_name, foreign_key: :type_name
+  
+    enum category: { tops: 0, bottoms: 1, outerwear: 2}
+  
+    has_one_attached :image
+
+    validates :user_id, presence: true
+    validates :last_worn_on, presence: true
+    validate :last_worn_on_is_date?
+    validates :type_name, presence: true
+    validates :category, presence: true
+  
+    # 必要な他のバリデーションを追加
+    # TODO:画像ファイルの制限, type_nameがcloth_typeのnameに存在するか
+
+    # def category_matches_type
+    #     if cloth_type && category != cloth_type.category
+    #       errors.add(:category, "must be #{cloth_type.category} for #{cloth_type.name}")
+    #     end
+    # end
+
+    private
+    def last_worn_on_is_date?
+    errors.add(:last_worn_on, 'must be a valid date') unless last_worn_on.is_a?(Date)
+    end
 end
--- app/models/cloth_type.rb
+++ app/models/cloth_type.rb
@@ -1,2 +1,6 @@
 class ClothType < ApplicationRecord
+    has_many :cloths, primary_key: :type_name, foreign_key: :type_name
+
+    validates :type_name, presence: true, uniqueness: true
+    validates :category, presence: true
 end
