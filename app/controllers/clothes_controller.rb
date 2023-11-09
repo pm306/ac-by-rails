1448a9f873ac42cff55fff40f8582d7da8a05cb0
pm <ampm65535@gmail.com>
2023-11-09 23:05:15 +0900

    服をデータベースに新規登録（bugfix）

--- app/controllers/clothes_controller.rb
+++ app/controllers/clothes_controller.rb
@@ -10,11 +10,11 @@ class ClothesController < ApplicationController
 
   def create
     @cloth = Cloth.new(cloth_params)
-    @cloth.user_id = 1
+    @cloth.user_id = current_user.id
     @cloth.last_worn_on = Date.parse('2000-01-01')
     if @cloth.save
-      flash.now[:success] = "服が登録されました"
-      render :new
+      flash[:success] = "服が登録されました"
+      redirect_to closet_add_path
     else
       flash.now[:error] = "服の登録に失敗しました"
       render :new
