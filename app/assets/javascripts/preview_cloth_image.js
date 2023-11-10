0bf5cab2892fcd28fef320904c6d2fe61fad7332
pm <ampm65535@gmail.com>
2023-11-10 20:55:12 +0900

    refactor-js

--- app/assets/javascripts/preview_cloth_image.js
+++ app/assets/javascripts/preview_cloth_image.js
@@ -1,23 +1,47 @@
 document.addEventListener("DOMContentLoaded", function() {
+    initializeFileInput();
+});
+
+function initializeFileInput() {
     // input要素を見つけます
     let input = document.querySelector('input[type=file]');
-    
+
     // input要素が変更された時のイベントリスナーを追加します
     input.addEventListener('change', function(e) {
+        handleFileChange(e);
+    });
+}
+
+function handleFileChange(e) {
     // 選択されたファイルを取得します
     let file = e.target.files[0];
+    if (!file) {
+        console.error('No file selected');
+        return;
+    }
+
+    // FileReaderのインスタンスを作成します
     let reader = new FileReader();
-    
+
     // ファイルが読み込まれたときのイベントリスナーを追加します
     reader.onload = function(e) {
-        // 読み込まれたファイルのデータをsrc属性として画像にセットします
-        let img = document.querySelector('#image-preview img');
-        img.src = e.target.result;
-        // 画像を表示状態にします
-        document.getElementById('image-preview').style.display = 'block';
+        displayImage(e.target.result);
     };
-    
+
+    // エラーハンドリングを追加します
+    reader.onerror = function() {
+        console.error('Error occurred while reading the file');
+    };
+
     // ファイルを読み込みます
     reader.readAsDataURL(file);
-    });
-});
+}
+
+function displayImage(imageData) {
+    // 画像を表示する要素を取得し、src属性を設定します
+    let img = document.querySelector('#image-preview img');
+    img.src = imageData;
+
+    // 画像を表示状態にします
+    document.getElementById('image-preview').style.display = 'block';
+}
