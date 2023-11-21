884940cf184a6686548c7b4035d3871f71f562fb
pm <ampm65535@gmail.com>
2023-11-21 14:37:28 +0900

    ロジック詳細ページに情報を表示する

--- app/models/cloth_group.rb
+++ app/models/cloth_group.rb
@@ -1,6 +1,7 @@
 class ClothGroup < ApplicationRecord
   has_many :cloths,                 dependent: :destroy
   has_many :cloth_group_selections, dependent: :destroy
+  has_many :outfit_selection_rules, through: :cloth_group_selections
 
   validates :name, presence: true, uniqueness: true
 end
--- app/models/outfit_selection_rule.rb
+++ app/models/outfit_selection_rule.rb
@@ -1,5 +1,6 @@
 class OutfitSelectionRule < ApplicationRecord
   has_many :cloth_group_selections, dependent: :destroy
+  has_many :cloth_groups, through: :cloth_group_selections
 
   validates :name,     presence: true, uniqueness: true
   validates :priority, presence: true
