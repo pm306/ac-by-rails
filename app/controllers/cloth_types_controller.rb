d875c243b42119ce0f150305d3fada58dac85465
pm <ampm65535@gmail.com>
2023-11-21 22:26:30 +0900

    服の分類を追加するフォームを作成

--- app/controllers/cloth_types_controller.rb
+++ app/controllers/cloth_types_controller.rb
@@ -1,7 +1,27 @@
 class ClothTypesController < ApplicationController
-  before_action :require_login, only: [:index]
+  before_action :require_login, only: [:index, :new, :create]
 
   def index
     @cloth_types = ClothType.includes(:cloth_group).all
+  end
+
+  def new
+    @cloth_type = ClothType.new
+    @cloth_groups = ClothGroup.all
+  end
+
+  def create
+    @cloth_type = ClothType.new(cloth_type_create_params)
+    if @cloth_type.save
+      flash[:success] = "分類を追加できました。"
+      redirect_to cloth_types_url
+    else
+      flash[:error] = "分類を追加できませんでした。"
+      redirect_to new_cloth_type_url
+    end
+  end
+
+  def cloth_type_create_params
+    params.require(:cloth_type).permit(:name, :cloth_group_id)
   end
 end
