c49880ff57b602da6b4ceba2f575a3b45a1b17c0
pm <ampm65535@gmail.com>
2023-11-29 20:37:34 +0900

    no message

--- db/migrate/20231109134401_add_cloth_type_id_to_cloths.rb
+++ db/migrate/20231109134401_add_cloth_type_id_to_cloths.rb
@@ -1,9 +1,5 @@
 class AddClothTypeIdToCloths < ActiveRecord::Migration[7.1]
   def change
-<<<<<<< HEAD
-    add_column :clothes, :cloth_type_id, :integer
-    remove_column :clothes, :type_name, :varchar
-=======
     unless column_exists?(:clothes, :cloth_type_id)
       add_column :clothes, :cloth_type_id, :integer
     end
@@ -11,6 +7,5 @@ class AddClothTypeIdToCloths < ActiveRecord::Migration[7.1]
     if column_exists?(:clothes, :type_name)
       remove_column :clothes, :type_name
     end
->>>>>>> bugfix-docker
   end
 end
