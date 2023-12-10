8446734718ee88503dc86fce66ce84cf5f356408
pm <ampm65535@gmail.com>
2023-12-10 18:15:58 +0900

    ビジネスロジックのリファクタリング

--- app/services/outfit_selector.rb
+++ app/services/outfit_selector.rb
@@ -3,7 +3,7 @@ class OutfitSelector
     @user = user
   end
 
-  # 服を選択するビジネスロジック
+  # ユーザー入力とルールに基づいて服装を選択する
   #
   # @param max_temprature [Integer] リクエストから受け取った最高気温
   # @param min_temprature [Integer] リクエストから受け取った最低気温
@@ -16,7 +16,7 @@ class OutfitSelector
 
   private
 
-  # 適用するルールidを取得する
+  # 与えられた気温に基づいて適用するルールidを取得する
   #
   # @param max_temp [Integer] リクエストから受け取った最高気温
   # @param min_temp [Integer] リクエストから受け取った最低気温
@@ -33,11 +33,11 @@ class OutfitSelector
   end
 
   # 与えられたルールIDに基づきClothGroupSelectionレコードを検索し、
-  # それらの各レコードから服グループの名前と選択回数を含むハッシュの配列を返す。
+  # それらの各レコードから服グループの名前と選択回数を含むハッシュの配列を返す
   #
   # @param rule_id [Integer] 検索するOutfitSelectionRuleのID
   # @return [Array<Hash>] 以下のキーを含むハッシュの配列:
-  #   - :cloth_group [String] 服グループのid
+  #   - :cloth_group_id [String] 服グループのid
   #   - :selection_count [Integer] 選択回数
   def find_cloth_group_selections(rule_id)
     ClothGroupSelection.includes(:cloth_group)
@@ -50,15 +50,35 @@ class OutfitSelector
     end
   end
 
-  # グループごとに服を決められた枚数だけランダム選択する
+  # 特定の服グループIDに対応する服タイプIDの配列を取得する
+  #
+  # @param [Integer] cloth_group_id 服グループのID
+  # @return [Array<Integer>, nil] 服タイプIDの配列、または服グループが存在しない場合はnil
+  def find_cloth_type_ids_for_group(cloth_group_id)
+    cloth_group = ClothGroup.find_by(id: cloth_group_id)
+    ClothType.where(cloth_group_id: cloth_group.id).pluck(:id) if cloth_group
+  end
+
+  # 与えられた服タイプIDのセットに基づいてランダムに服を選択する
+  #
+  # @param [Array<Integer>] cloth_type_ids 服タイプIDの配列
+  # @param [Integer] selection_count 選択する服の枚数
+  # @return [Array<Integer>] 選択された服のIDの配列
+  def select_clothes_for_types(cloth_type_ids, selection_count)
+    Cloth.where(cloth_type_id: cloth_type_ids)
+         .where(user_id: @user.id)
+         .where('last_worn_on IS NULL OR last_worn_on < ?', Date.yesterday)
+         .sample(selection_count).pluck(:id)
+  end
+
+  # 与えられた選択肢に基づいて服をランダムに選択する
+  #
+  # @param [Array<Hash>] selections 選択肢の配列（各ハッシュは :cloth_group_id と :selection_count を含む）
+  # @return [Array<Integer>] 選択された服のIDの配列
   def select_clothes_ids(selections)
     selections.map do |selection|
-      cloth_group = ClothGroup.find_by(id: selection[:cloth_group_id])
-      cloth_type_ids = ClothType.where(cloth_group_id: cloth_group.id).pluck(:id)
-      Cloth.where(cloth_type_id: cloth_type_ids)
-           .where(user_id: @user.id)
-           .where('last_worn_on IS NULL OR last_worn_on < ?', Date.yesterday)
-           .sample(selection[:selection_count]).pluck(:id)
-    end.flatten
+      cloth_type_ids = find_cloth_type_ids_for_group(selection[:cloth_group_id])
+      select_clothes_for_types(cloth_type_ids, selection[:selection_count]) if cloth_type_ids
+    end.compact.flatten
   end
 end
